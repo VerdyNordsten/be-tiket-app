e13bb9cb49ca4cfb8d293fd47f733e8b968b0ad2
ikkair <ikhlasul.kamal.i@gmail.com>
2023-03-11 02:28:44 +0700

    adding airlines entity

--- query.sql
+++ query.sql
@@ -1,6 +1,10 @@
+-- Database Init
+-- Delete database if same name exist
 CREATE DATABASE ticket_app;
 
+-- Create Enum
 CREATE TYPE title_enum AS ENUM ('mr', 'ms', 'mrs');
+-- Create Table (unused)
 CREATE TABLE users (
     id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -14,3 +18,23 @@ CREATE TABLE users (
     photo VARCHAR(255),
     role VARCHAR(20) DEFAULT 'customer'
 );
+
+-- Delete table if same name exist
+DROP TABLE IF EXISTS airlines;
+
+-- Creating table
+CREATE TABLE airlines (
+    id CHAR(40) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL, -- note not null
+    photo VARCHAR(100) NOT NULL, -- note not null
+    active BOOLEAN NOT NULL -- note not null
+);
+
+-- Dummy data
+INSERT INTO airlines(id, name, photo, active) 
+    VALUES
+        (1, 'Adam Air', 'photo.jpg', True),
+        (2, 'Aviastar', 'photo.jpg', True),
+        (3, 'Batavia Air', 'photo.jpg', True),
+        (4, 'Batik Air', 'photo.jpg', True),
+        (5, 'Citilink', 'photo.jpg', True);
