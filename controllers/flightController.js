01f44980d79680fe4ab2716f980a32cb6a63dde9
Verdy Nordsten <51946708+VerdyNordsten@users.noreply.github.com>
2023-03-15 06:57:54 +0800

    Update flightController.js
    unpexpected } get delete

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -64,7 +64,8 @@ const flightController = {
         limit,
         totalData,
         totalPage,
-
+      }
+      
       commonHelper.response(
         res,
         result.rows.map((data) => {
