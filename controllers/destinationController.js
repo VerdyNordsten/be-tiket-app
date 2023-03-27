9ad2075200e72c8e815cdbdd13fe2ea72c1b3888
ikkair <ikhlasul.kamal.i@gmail.com>
2023-03-27 13:59:17 +0700

    Destination revision

--- controllers/destinationController.js
+++ controllers/destinationController.js
@@ -7,6 +7,13 @@ const {v4: uuidv4} = require("uuid")
 // Import Helper for Template Response
 const commonHelper = require("../helper/common")
 
+// Import upload google
+const {
+    updatePhoto,
+    uploadPhoto,
+    deletePhoto,
+} = require("../config/googleDrive.config");
+
 const destinationController = {
   getAllDestinations: async (req, res) => {
     // Set params as const
@@ -42,31 +49,94 @@ const destinationController = {
   },
 
   addDestination: async (req, res) => {
+    // Validate Role
+    const { role } = req.payload
+    if (role !== "admin") {
+      return commonHelper.response(res, null, 403, "Only Admin are allowed to add Destination")
+    }
     // Generate Id
     req.body.queryId = uuidv4()
+    // Upload to google drive
+    let uploadResult
     try {
-      const insertResult = await destinationModel.insertDestination(req.body)
-      return commonHelper.response(res, insertResult.rows, 200, "Destination added" )
+      uploadResult = await uploadPhoto(req.file);
+      const parentPath = process.env.GOOGLE_DRIVE_PHOTO_PATH;
+      req.body.queryFilename = parentPath.concat(uploadResult.id);
     } catch (error) {
       console.log(error)
+      // Default if upload error
+      req.body.queryFilename = "photo.jpg";
+    }
+    // Declare variable for holding query result
+    let insertResult
+    try {
+      insertResult = await destinationModel.insertDestination(req.body)
+    } catch (error) {
+      console.log(error)
+      try {
+        if (uploadResult){
+          deletePhoto(uploadResult.id)
+        }
+      } catch (error) {
+        console.log(error)
+      }
       if (error.detail && error.detail.includes('already exists.')) {
         return commonHelper.response(res, null, 400, "Destination name already exist" )
       } else {
         return commonHelper.response(res, null, 500, "Failed to add destination" )
       }
     }
+    return commonHelper.response(res, insertResult.rows, 200, "Destination added" )
   }, 
 
   editDestination: async (req, res) => {
+    // Validate Role
+    const { role } = req.payload
+    if (role !== "admin") {
+      return commonHelper.response(res, null, 403, "Only Admin are allowed to edit Destination")
+    }
     // Set param id as const
     const queryId = req.params.id
     req.body.queryId = queryId
+    // Declare variable for holding query result
+    let selectResult
     try {
-      const insertResult = await destinationModel.updateDestination(req.body)
-      if (insertResult.rowCount < 1){
+      selectResult = await destinationModel.selectDetailDestination(queryId)
+      if (selectResult.rowCount < 1){
         return commonHelper.response(res, null, 404, "Destination not found" )
       }
-      return commonHelper.response(res, insertResult.rows, 200, "Destination edited" )
+    } catch (error) {
+      console.log(error)
+      return commonHelper.response(res, null, 500, "Failed to get data destination" )
+    }
+    // Update the old photo
+    const oldPhoto = selectResult.rows[0].photo;
+    if (req.file){
+      try {
+        if (oldPhoto != "undefined" && oldPhoto != "photo.jpg" && oldPhoto != "") {
+          const oldPhotoId = oldPhoto.split("=")[1];
+          const updateResult = await updatePhoto(
+              req.file,
+              oldPhotoId
+          );
+          const parentPath = process.env.GOOGLE_DRIVE_PHOTO_PATH;
+          req.body.queryFilename = parentPath.concat(updateResult.id);
+        } else {
+          const uploadResult = await uploadPhoto(req.file);
+          const parentPath = process.env.GOOGLE_DRIVE_PHOTO_PATH;
+          req.body.queryFilename = parentPath.concat(uploadResult.id);
+        }
+      } catch (error) {
+        console.log(error)
+        return commonHelper.response(res, null, 500, "Failed to update destination photo" )
+      }
+    } else {
+      req.body.queryFilename = oldPhoto
+    }
+    // Update other field
+    try {
+      const updateResult = await destinationModel.updateDestination(req.body)
+      return commonHelper.response(res, updateResult.rows, 200, "Destination edited" )
     } catch (error) {
       console.log(error)
       if (error.detail && error.detail.includes('already exists.')) {
@@ -78,14 +148,32 @@ const destinationController = {
   },
 
   deleteDestination: async (req, res) => {
+    // Validate Role
+    const { role } = req.payload
+    if (role !== "admin") {
+      return commonHelper.response(res, null, 403, "Only Admin are allowed to delete Destination")
+    }
     // Set param id as const
     const queryId = req.params.id
+    // Declare variable for holding query result
+    let selectResult
+    try {
+      selectResult = await destinationModel.selectDetailDestination(queryId)
+      if (selectResult.rowCount < 1){
+        return commonHelper.response(res, null, 404, "Destination not found" )
+      }
+    } catch (error) {
+      console.log(error)
+      return commonHelper.response(res, null, 500, "Failed to get data destination" )
+    }
     // Declare variable for holding query result
     let deleteResult
     try {
       deleteResult = await destinationModel.deleteDestination(queryId)
-      if (deleteResult.rowCount < 1){
-        return commonHelper.response(res, null, 404, "Destination not found" )
+      const oldPhoto = selectResult.rows[0].photo;
+      if (oldPhoto != "undefined" && oldPhoto != "photo.jpg" && oldPhoto != "") {
+        const oldPhotoId = oldPhoto.split("=")[1];
+        await deletePhoto(oldPhotoId);
       }
       return commonHelper.response(res, deleteResult.rows, 200, "Destination deleted" )
     } catch (error) {
--- routes/destinationRoutes.js
+++ routes/destinationRoutes.js
@@ -5,10 +5,16 @@ const router = express.Router()
 // Import controller functions
 const destinationController = require("../controllers/destinationController")
 
+// Import upload
+const upload = require("../middleware/upload.js");
+
+// Import verif token
+const { verifyToken } = require("../middleware/auth")
+
 router.get("/", destinationController.getAllDestinations)
 router.get("/:id", destinationController.getDetailDestination)
-router.post("/", destinationController.addDestination)
-router.put("/:id", destinationController.editDestination)
-router.delete("/:id", destinationController.deleteDestination)
+router.post("/", verifyToken, upload.single("photo"), destinationController.addDestination)
+router.put("/:id", verifyToken, upload.single("photo"), destinationController.editDestination)
+router.delete("/:id", verifyToken, destinationController.deleteDestination)
 
 module.exports = router
