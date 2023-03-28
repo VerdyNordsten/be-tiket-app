13b2d1b8b69e0b5f73ad6c3ea8aa13ba6adb6426
mhmdiamd <darmawanilham34@gmail.com>
2023-03-28 08:35:34 +0700

    fix origin

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(cors({
-  origin: process.env.ORIGIN_DOMAIN,
+  origin: [process.env.ORIGIN_DOMAIN, 'http://localhost:5173'],
   methods: "GET, PUT, POST, DELETE",
   credentials: true
 }))
